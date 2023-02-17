add4d510e3f2fc05d97f23af7b556f418e38fe5d
Raghav <kumarraghav71950@gmail.com>
2023-02-17 22:10:05 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/App.css';
+
 const App = () => {
   const [shapes, setShapes] = useState([]);
   const [selected, setSelected] = useState("square");
@@ -13,16 +14,17 @@ const App = () => {
 
   return (
     <div id="main">
-      <div id="shape-creator"></div>
-      <select onChange={(e) => setSelected(e.target.value)} value={selected}>
-        <option id="option-square">Square</option>
-        <option id="option-circle">Circle</option>
-      </select>
-      <button onClick={handleAddShape}>Add Shape</button>
+      <div id="shape-creator">
+        <select onChange={(e) => setSelected(e.target.value)} value={selected}>
+          <option id="option-square">Square</option>
+          <option id="option-circle">Circle</option>
+        </select>
+        <button onClick={handleAddShape}>Add Shape</button>
+      </div>
       <div id="shapes-holder">
-        {shapes.map((eachShape, index) => (
-          <div className={eachShape} key={index}>
-            {index}
+        {shapes.map((shape, idx) => (
+          <div className={shape} key={idx}>
+            {idx}
           </div>
         ))}
       </div>
